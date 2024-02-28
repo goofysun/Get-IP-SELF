49ea93b08c56a0ac418d2cad60f3ebea1d2dacea
Jun Liu <goofysun@gmail.com>
2024-02-28 11:38:50 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -4,8 +4,8 @@ echo "#Last update: $last";
 echo "/ip firewall address-list remove [/ip firewall address-list find list=NoNAT]";
 echo "/ip firewall address-list";
 
-for range in $( curl --silent -X POST -H 'Connection: close' "https://stat.ripe.net/data/country-resource-list/data.json?resource=IR&v4_format=prefix" | jq .data.resources.ipv4[] | sed 's/"//g' ); do
-    echo ":do { add address=$range list=NoNAT} on-error={}";
+for range in $( curl --silent -X POST -H 'Connection: close' "https://raw.githubusercontent.com/naruto522ru/ipranges/main/telegram/ipv4_merged.txt"); do
+    echo ":do { add address=$range list=gfw} on-error={}";
 done;
 
-echo ":do { add address=10.0.0.0/8 list=NoNAT} on-error={}";
+echo ":do { add address=10.0.0.0/8 list=gfw} on-error={}";
