eea420fa0ea61b006d3cd1f7e0ccfb1a3e610263
Jun Liu <goofysun@gmail.com>
2025-06-13 11:31:39 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -4,14 +4,12 @@ echo "#Last update: $last";
 echo "/ip firewall address-list remove [/ip firewall address-list find list=gfw]";
 echo "/ip firewall address-list";
 
-array=("https://raw.githubusercontent.com/naruto522ru/ipranges/main/telegram/ipv4_merged.txt" 
-        "https://raw.githubusercontent.com/naruto522ru/ipranges/main/google/ipv4_merged.txt" 
-        "https://raw.githubusercontent.com/naruto522ru/ipranges/main/googlecloud/ipv4_merged.txt"
-        "https://raw.githubusercontent.com/naruto522ru/ipranges/main/chatgpt/ipv4_merged.txt"
-        "https://raw.githubusercontent.com/naruto522ru/ipranges/main/facebook/ipv4_merged.txt"
-        "https://raw.githubusercontent.com/naruto522ru/ipranges/main/twitter/ipv4_merged.txt"
-        "https://raw.githubusercontent.com/naruto522ru/ipranges/main/youtube/ipv4_merged.txt"
-        "https://raw.githubusercontent.com/lord-alfred/ipranges/main/all/ipv4_merged.txt")
+array=("https://raw.githubusercontent.com/lord-alfred/ipranges/main/openai/ipv4_merged.txt" 
+        "https://raw.githubusercontent.com/lord-alfred/ipranges/main/google/ipv4_merged.txt" 
+        "https://raw.githubusercontent.com/lord-alfred/ipranges/main/telegram/ipv4_merged.txt"
+        "https://raw.githubusercontent.com/lord-alfred/ipranges/main/facebook/ipv4_merged.txt"
+        "https://raw.githubusercontent.com/lord-alfred/ipranges/main/twitter/ipv4_merged.txt"
+        )
  
 # 遍历字符串数组
 for element in "${array[@]}" ; do
